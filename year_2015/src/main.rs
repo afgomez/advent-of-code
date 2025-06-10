f5e755a8087e8811d4a5c31ce5c5f59da3328f01
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2025-06-10 21:24:45 +0200

    Bootstrap year 2015

---
+++ year_2015/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Read the first argument and parse it as a number
+    let number = std::env::args()
+        .nth(1)
+        .expect("Please provide a number as an argument");
+    let number = number
+        .parse::<i32>()
+        .expect("The provided argument is not a valid number");
+
+    match number {
+        _ => println!("No solution found"),
+    }
+}
